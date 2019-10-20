b4cb1aafe36c963b4cd7fddc3a1354575ccba2da
talentceffort <talentceffort@gmail.com>
2019-10-20 13:02:45 +0900

    modify hanoi

--- Recursion/Hanoi.py
+++ Recursion/Hanoi.py
@@ -10,7 +10,7 @@ def hanoi(num_disks, start_peg, end_peg):
     other_peg = 6 - start_peg - end_peg
     hanoi(num_disks - 1, start_peg, other_peg)
     move_disk(num_disks, start_peg, end_peg)
-    hanoi(num_disks -1, other_peg, end_peg)
+    hanoi(num_disks - 1, other_peg, end_peg)
 
 # 1. 가장 큰 원판을 제외하고 나머지 원판들을 start_peg에서 other_peg로 이동
 # 2. 가장 큰 원판을 start_peg에서 end_peg로 이동
@@ -20,6 +20,8 @@ def hanoi(num_disks, start_peg, end_peg):
 #hanoi(1, 1, 3)
 #hanoi(2, 1, 3)
 hanoi(3, 1, 3)
+
+#hanoi(3, 1, 3) hanoi(2, 1, 2), hanoi(1, 1, 3) hanoi(1, 3, 2) hanoi(2, 2, 3) hanoi(1, 2, 1) hanoi(1, 1, 3) 순으로 실행.
 
 # 원판이 2개일 때.
 # 1번 원판을 1번 기둥에서 2번 기둥으로 이동
